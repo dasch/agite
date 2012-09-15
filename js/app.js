9a0a5ee15dc6809c42e3580e0e28ed93c5edcf29
Daniel Schierbeck <dasch@zendesk.com>
2012-09-15 17:41:16 +0200

    Do a bunch of work

--- js/app.js
+++ js/app.js
@@ -1,7 +1,76 @@
-var App = Em.Application.create();
+var App = Em.Application.create({
+  repo_path: "https://api.github.com/repos/dasch/agile-glory",
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
+  ready: function() {
+    this._super();
+
+    this.getMilestone();
+
+    var sectionView = Em.View.create({
+      templateName: "section",
+      stories: App.issuesController
+    });
+
+    sectionView.append();
+  },
+
+  getMilestone: function() {
+    App.milestoneController.refresh();
+  }
+});
+
+App.Issue = Em.Object.extend({
+  title: null,
+  number: null,
+  assignee: null,
+  state: "open"
+});
+
+App.issuesController = Em.ArrayController.create({
+  content: [],
+
+  addIssue: function(issue) {
+    this.pushObject(issue);
+  },
+
+  refresh: function() {
+    var self = this;
+    var milestoneNumber = App.milestoneController.milestone.number;
+
+    if (milestoneNumber === undefined) return;
+
+    var state = "open";
+    var endpoint = App.repo_path + "/issues";
+    var params = { milestone: milestoneNumber, state: state };
+
+    $.getJSON(endpoint, params, function(issues) {
+      for (var i = 0; i < issues.length; i++) {
+        var issue = issues[i];
+        console.log(issue);
+        self.addIssue(issue);
+      }
+    });
+  }
+});
+
+App.milestoneController = Em.Object.create({
+  milestone: Em.Object.create({
+    number: null,
+    title: "N/A"
+  }),
+
+  refresh: function() {
+    var self = this;
+    var params = { sort: "due_at", direction: "asc", limit:1 };
+    var endpoint = App.repo_path + "/milestones";
+
+    $.getJSON(endpoint, params, function(milestones) {
+      var milestone = milestones[0];
+
+      self.milestone.set("title", milestone.title);
+      self.milestone.set("number", milestone.number);
+
+      App.issuesController.refresh();
+    });
   }
 });

f2a4ead727ee5c49697c6ec8387c995f0d197187
Daniel Schierbeck <dasch@zendesk.com>
2012-09-18 11:13:36 +0200

    Add authentication

--- git_board.rb
+++ git_board.rb
@@ -1,7 +1,49 @@
 require 'sinatra'
+require 'omniauth'
+require 'omniauth-github'
 
 class GitBoard < Sinatra::Base
+  configure(:development) do
+    set :session_secret, "something"
+  end
+
+  use Rack::Session::Cookie
+
+  use OmniAuth::Builder do
+    provider :developer
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  end
+
+  enable :sessions
+
   get '/' do
-    erb :index
+    @token = session['token']
+
+    if @token.nil?
+      redirect '/auth'
+    else
+      erb :index
+    end
+  end
+
+  get '/auth' do
+    if development?
+      redirect '/auth/developer'
+    else
+      redirect '/auth/github'
+    end
+  end
+
+  post '/auth/:provider/callback' do
+    if params[:provider] == 'github'
+      auth = request.env['omniauth.auth']
+      token = auth['credential']['token']
+    else
+      token = "foo"
+    end
+
+    session['token'] = token
+
+    redirect '/'
   end
 end
